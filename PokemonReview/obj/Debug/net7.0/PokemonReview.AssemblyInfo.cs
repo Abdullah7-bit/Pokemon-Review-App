5887d0e2c3fff8e815fdea087b45213f374da336
Abdullah Imran <abdullahimran999@gmail.com>
2024-07-16 17:30:28 +0500

    changes in build files

--- PokemonReview/obj/Debug/net7.0/PokemonReview.AssemblyInfo.cs
+++ PokemonReview/obj/Debug/net7.0/PokemonReview.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokemonReview")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f36b381055253311b2ef20fa054a42441aac06e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+843ab5aaa1699c37385d7727202fd9faf72d0323")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokemonReview")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokemonReview")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
